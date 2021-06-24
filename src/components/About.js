fcfa7f738200a73a53f3925efe00b2b6fb8c354e
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-06-24 11:19:24 -0400

    Update styles

--- src/components/About.js
+++ src/components/About.js
@@ -6,31 +6,33 @@ import {SiJavascript} from 'react-icons/si'
 const About = () => {
     return (
         <section id='about'>
-            <div className="about-img">
-                <img src={Adriana} alt="Adriana" />
-            </div>
-            <div className="about-text">
-                    <div className="about-me">
-                        <h2>About</h2>
-                        <p>Hey there! I am Adriana, a front-end developer based in Canada. 
-                            My greatest strength as a developer is my ability to problem solve.
-                            I genuinely find the challenge of tackling new projects to be fun, am fascinted by how many different ways we can solve the same problem and am hooked on the feeling of satisfaction I get once I solve a bug.
-                        </p>
-                    </div>
-                    <div className="about-toolkit">
-                        <h3>My Toolkit</h3>
-                        <ul className='about-toolkit-icon'>
-                            <li><FaHtml5 className='about-icon'/></li>
-                            <li><FaCss3Alt className='about-icon'/></li>
-                            <li><FaSass className='about-icon'/></li>
-                            <li><SiJavascript className='about-icon'/></li>
-                            <li><FaReact className='about-icon'/></li>
-                        </ul>
-                    </div>
-                    <div class="about-resume">
-                        <h3>My Resume</h3>
-                        <div class="about-resume-icons">
-                            <a href="AdrianaCalvo-Matos_Resume.pdf" target="_blank"><FaFileDownload className='about-icon about-download'/></a>
+            <div className="container">
+                <div className="about-img">
+                    <img src={Adriana} alt="Adriana" />
+                </div>
+                <div className="about-text">
+                        <div className="about-me">
+                            <h2>About</h2>
+                            <p>Hey there! I am Adriana, a front-end developer based in Canada. 
+                                My greatest strength as a developer is my ability to problem solve.
+                                I genuinely find the challenge of tackling new projects to be fun, am fascinted by how many different ways we can solve the same problem and am hooked on the feeling of satisfaction I get once I solve a bug.
+                            </p>
+                        </div>
+                        <div className="about-toolkit">
+                            <h3>My Toolkit</h3>
+                            <ul className='about-toolkit-icon'>
+                                <li><FaHtml5 className='about-icon'/></li>
+                                <li><FaCss3Alt className='about-icon'/></li>
+                                <li><FaSass className='about-icon'/></li>
+                                <li><SiJavascript className='about-icon'/></li>
+                                <li><FaReact className='about-icon'/></li>
+                            </ul>
+                        </div>
+                        <div class="about-resume">
+                            <h3>My Resume</h3>
+                            <div class="about-resume-icon">
+                                <a href="AdrianaCalvo-Matos_Resume.pdf" target="_blank"><FaFileDownload className='about-icon about-download'/></a>
+                            </div>
                         </div>
                     </div>
                 </div>
