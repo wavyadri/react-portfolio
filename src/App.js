5f0b1b182e1097e48f5a9e8ff14ee015c4eaab5e
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-06-27 14:20:42 -0400

    Completed footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Typewriter from './components/Typewriter';
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import { BrowserRouter, Link } from 'react-router-dom';
 
 
@@ -17,6 +18,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </BrowserRouter>
   )
 }
