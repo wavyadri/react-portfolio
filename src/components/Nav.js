228a40081f248bb5a071139a0e06ec15e737160f
Adriana Calvo-Matos <63519712+wavyadri@users.noreply.github.com>
2021-07-03 00:12:27 -0400

    Update styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,39 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import logo from '../Adriana-logo.svg'
+import {FaBars} from 'react-icons/fa'
 
 const Nav = () => {
+    const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null); // for ul
+    const linksRef = useRef(null); // for li
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        if(showLinks) {
+            linksContainerRef.current.style.height = `${linksHeight}px`
+        } else {
+            linksContainerRef.current.style.height = '0px'
+        }
+        
+    }, [showLinks])
+
     return (
         <nav> 
             <div className='nav-main'>
                 <div className='nav-logo'>
                     <Link to='/'><img src={logo} alt="logo" /></Link>
+                    <button className='nav-toggle'><FaBars/></button>
                 </div>
-                <ul className='nav-links'>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><HashLink scroll to='/#about'>About</HashLink></li>
-                    <li><HashLink scroll to='/#projects'>Projects</HashLink></li>
-                    <li><HashLink to='/#contact'>Contact</HashLink></li>
-                </ul>
+                <div className='nav-links-container' ref={linksContainerRef}>
+                    <ul className='nav-links' ref={linksRef}>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><HashLink scroll to='/#about'>About</HashLink></li>
+                        <li><HashLink scroll to='/#projects'>Projects</HashLink></li>
+                        <li><HashLink to='/#contact'>Contact</HashLink></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
